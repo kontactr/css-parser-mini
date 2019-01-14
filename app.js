318ac2941382cbc900e48c4f9af058ae5b258d5f
kontactr <gandhiyash1381997@gmail.com>
2019-01-14 22:58:08 +0530

    preprocessed string added

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ h2 h3 {
     prop2: value1 value2;
 }
 
-h1,h2 .main #id{
+h1 , h2 .main #id{
     prop1: value2;
     prop2 : value4;
 }
 
-h1::n-th-of-type(5n){
+h1 :: n-th-of-type(5n){
     p1:p1;
 }
 
@@ -33,6 +33,7 @@ h2:n-p{
 @media only screen(maxWidth: 500px) and (min-width: 700px){
     h1{
         prop1: value2;
+        prop3: value2 value4;
     }
 }
 
--- parser/SelectorParser.js
+++ parser/SelectorParser.js
@@ -1,7 +1,75 @@
 function selectorParser(cssObject){
 
+    function alphanumeric(inputtxt)
+    { 
+        var letters = /^[0-9a-zA-Z]+$/;
+        if(inputtxt.match(letters))
+            return true;
+        else
+            return false;
+    }
 
-    return cssObject;
+    function preprocessed(queryString){
+        let newQueryString = ""
+        let spaceBuffered = false;
+        let operatorBuffered = false;
+        let operatorSet = [",",".","#","+","~",":"]
+        for (let character of queryString){
+            if(character === " "){
+                spaceBuffered = true;
+            }else if(operatorSet.includes(character)){
+                newQueryString += character;
+                spaceBuffered = false;
+                operatorBuffered = true;
+            }else {
+                if(spaceBuffered && !operatorBuffered){
+                    newQueryString += " ";
+                    spaceBuffered = false;
+                    operatorBuffered = false;
+                }
+                newQueryString += character;
+            }
+        }
+        return newQueryString;
+    }
+
+    function internalParser(queryString , queryObject){
+
+        
+        let queryCache = '';
+        let spaceEncountered = false;
+        let stack = [];
+        let type = "";
+
+        queryObject["selectorObject"] = {};
+
+        stack.push(queryObject["selectorObject"]);
+        let currentObject = queryObject["selectorObject"];
+        currentObject.children = [];
+
+        queryString = preprocessed(queryString);
+        console.log(queryString);
+
+        for(let character of queryString){
+                break;
+
+            
+        }
+    }
+
+
+    function roundTrip(cssObject){
+        for (let cssSelector of cssObject){
+            
+            if(cssSelector.directiveSelector){
+                 roundTrip(cssSelector.cssArrayObjects);
+                 continue;
+            }
+            internalParser(cssSelector.selector , cssSelector);
+        }
+    }
+    roundTrip(cssObject);
+    
 }
 
 module.exports = {
